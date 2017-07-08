8020c4f9030994e60d3508a02c352f7fabbacd8e
Yakoloi <yakoloi@gmail.com>
2017-07-08 15:48:24 -0700

    game coming along

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,64 @@
 var targetNumber;
-var crystalArray;
-var crystalNumber
+var crystalArray = [];
 var score = 0;
 var win = 0;
 var loss = 0;
+var currentNum = 0;
 
-crystalNumber = Math.floor(Math.random() * 12) + 1;
-console.log("crystalNumber" +  crystalNumber)
+function init() {
+  targetNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+  while (crystalArray.length < 4) {
+    var crystalNumber = Math.floor(Math.random() * 12) + 1;
+    if (crystalArray.indexOf(crystalNumber) > -1) continue;
+    crystalArray[crystalArray.length] = crystalNumber;
+  }
+  currentNum = 0;
+  $("#target").text("Number: " + targetNumber);
+  console.log("crystalArray: " + crystalArray);
+  console.log("set this crystal array index to ruby" + crystalArray[0]);
 
-targetNumber = Math.floor(Math.random() * 120) + 19
-console.log("targetNumber" + targetNumber)
+}
+
 
 $(document).ready(function() {
+  //generate unique random numbers in an array for the crystals
+  init();
 
-//generate unique random numbers in an array for the crystals
-crystalArray = [];
-while(crystalArray.length < 4) {
-  var randomNumber = Math.floor(Math.random() * 12 ) + 1;
-  if(crystalArray.indexOf(randomNumber) > -1) continue;
-  crystalArray[crystalArray.length] = randomNumber;
-}
-console.log("crystalArray" + crystalArray)
+
+  $(".crystals").click(function() {
+    var crystalValue = $(this).attr('id');
+
+      if (currentNum > targetNumber) {
+        alert("You lose");
+        loss ++;
+        targetNumber = "";
+        init();
+
+      }
+
+      else if (crystalValue === "ruby") {
+      currentNum = currentNum + crystalArray[0];
+      console.log("currentNum: " + currentNum);
+      $("#whiteboard").text("Current Number: " + currentNum);
+    }
+      else if (crystalValue === "beryl") {
+      currentNum = currentNum + crystalArray[1];
+      console.log("currentNum: " + currentNum);
+      $("#whiteboard").text("Current Number: " + currentNum);
+    }
+      else if (crystalValue === "emerald") {
+      currentNum = currentNum + crystalArray[2];
+      console.log("currentNum: " + currentNum);
+      $("#whiteboard").text("Current Number: " + currentNum);
+    }
+      else if (crystalValue === "sapphire") {
+      currentNum = currentNum + crystalArray[3];
+      console.log("currentNum: " + currentNum);
+      $("#whiteboard").text("Current Number: " + currentNum);
+    }
+
+  });
+
 
 
 });
